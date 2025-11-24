32858c6d08841a19eb3d6676cb8edd32fc2d4cbd
Konstantin <kostos97@gmail.com>
2025-11-24 03:14:53 +0300

    # Попытка поправить жуткий Webflux added: Расширено логирование

--- src/main/java/com/example/tgbot/web/RecordInfoResponse.java
+++ src/main/java/com/example/tgbot/web/RecordInfoResponse.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @Setter
+@ToString
 public class RecordInfoResponse {
     private int code;
     private String message;
