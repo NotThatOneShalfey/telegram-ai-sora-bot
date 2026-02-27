235ef28f42b14f22d1d44957dd3c3e466ad9cc34
Konstantin <kostos97@gmail.com>
2026-02-27 22:51:13 +0300

    # Правки багов

--- src/main/java/com/example/tgbot/telegram/TgBot.java
+++ src/main/java/com/example/tgbot/telegram/TgBot.java
@@ -92,8 +92,8 @@ public class TgBot extends TelegramWebhookBot {
             User user = userService.findOrCreateUser(update.getMessage().getChatId());
             // Проверка на дев билд
             if (devBuild && (user.getId() != 1 && user.getId() != 2 && user.getId() != 4)) {
-
-            } else { return; }
+                return;
+            }
             UserSession userSession = sessions.computeIfAbsent(update.getMessage().getChatId().toString(), k -> new UserSession(user));
             if (update.hasCallbackQuery()) {
                 log.trace("update has CallbackQuery");
