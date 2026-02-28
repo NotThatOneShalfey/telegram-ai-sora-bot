0ec83d6978dd35bfd4d397d2196618cad93693b3
Konstantin <kostos97@gmail.com>
2026-02-28 03:45:48 +0300

    # Правки багов

--- src/main/java/com/example/tgbot/telegram/buttons/enums/PaidPackageEnum.java
+++ src/main/java/com/example/tgbot/telegram/buttons/enums/PaidPackageEnum.java
@@ -8,30 +8,23 @@ import java.util.Optional;
 @Getter
 public enum PaidPackageEnum {
 
-    PACKAGE_100("\"100 монет\"", 100, 100, "package_100"),
-    PACKAGE_550("\"550 монет\"", 500, 550, "package_550"),
-    PACKAGE_1200("\"1200 монет\"", 1000, 1200, "package_1200"),
-    PACKAGE_6500("\"6500 монет\"", 5000, 6500, "package_6500");
+    PACKAGE_100("\"100 монет\" - 100 ₽", 100, 100),
+    PACKAGE_550("\"550 монет\" - 500 ₽", 500, 550),
+    PACKAGE_1200("\"1200 монет\" - 1000 ₽", 1000, 1200),
+    PACKAGE_6500("\"6500 монет\" - 5000 ₽", 5000, 6500);
 
     private String buttonName;
     private Integer packagePrice;
     private Integer packageAmount;
-    private String buttonCallback;
 
-    PaidPackageEnum(String buttonName, Integer packagePrice, Integer packageAmount, String buttonCallback) {
+    PaidPackageEnum(String buttonName, Integer packagePrice, Integer packageAmount) {
         this.buttonName = buttonName;
         this.packagePrice = packagePrice;
         this.packageAmount = packageAmount;
-        this.buttonCallback = buttonCallback;
     }
 
     public static Integer getPackagePriceByName(String externalName) {
         Optional<PaidPackageEnum> pack = Arrays.stream(PaidPackageEnum.values()).filter(pp -> pp.buttonName.equalsIgnoreCase(externalName)).findFirst();
         return pack.map(PaidPackageEnum::getPackageAmount).orElse(null);
-    }
-
-    public static PaidPackageEnum getPackageByCallback(String cb) {
-        Optional<PaidPackageEnum> pack = Arrays.stream(PaidPackageEnum.values()).filter(pp -> pp.buttonCallback.equalsIgnoreCase(cb)).findFirst();
-        return pack.orElse(null);
     }
 }
