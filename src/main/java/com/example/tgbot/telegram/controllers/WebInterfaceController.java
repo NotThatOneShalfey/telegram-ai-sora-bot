25874c793793d2924cd348590a1fa7c7064af4a6
Konstantin <kostos97@gmail.com>
2026-03-04 16:32:26 +0300

    # Правки багов

--- src/main/java/com/example/tgbot/telegram/controllers/WebInterfaceController.java
+++ src/main/java/com/example/tgbot/telegram/controllers/WebInterfaceController.java
@@ -3,6 +3,9 @@ package com.example.tgbot.telegram.controllers;
 import com.example.tgbot.models.configurations.dto.InterfaceDTORequest;
 import com.example.tgbot.models.enums.GenerationModel;
 import com.example.tgbot.telegram.TgBot;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,30 +18,35 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class WebInterfaceController {
     private final TgBot tgBot;
+    private final ObjectMapper mapper = new JsonMapper();
 
     @GetMapping("/example/kling")
-    public ResponseEntity<String> getKlingOptionsExample(@RequestParam String userName) {
+    public ResponseEntity<String> getKlingOptionsExample(@RequestParam(name = "userName") String userName) {
         return new ResponseEntity<>(tgBot.processExampleRequest(userName, GenerationModel.KLING_3_0), HttpStatus.OK);
     }
 
     @GetMapping("/example/sora2")
-    public ResponseEntity<String> getSoraOptionsExample(@RequestParam String userName) {
+    public ResponseEntity<String> getSoraOptionsExample(@RequestParam(name = "userName") String userName) {
         return new ResponseEntity<>(tgBot.processExampleRequest(userName, GenerationModel.SORA_2), HttpStatus.OK);
     }
 
     @GetMapping("/example/suno")
-    public ResponseEntity<String> getSunoOptionsExample(@RequestParam String userName) {
+    public ResponseEntity<String> getSunoOptionsExample(@RequestParam(name = "userName") String userName) {
         return new ResponseEntity<>(tgBot.processExampleRequest(userName, GenerationModel.KLING_3_0), HttpStatus.OK);
     }
 
     @GetMapping("/example/nanobanana")
-    public ResponseEntity<String> getNanoBananaOptionsExample(@RequestParam String userName) {
+    public ResponseEntity<String> getNanoBananaOptionsExample(@RequestParam(name = "userName") String userName) {
         return new ResponseEntity<>(tgBot.processExampleRequest(userName, GenerationModel.KLING_3_0), HttpStatus.OK);
     }
 
     @PostMapping("/generate")
-    public ResponseEntity<HttpStatus> generate(@RequestBody InterfaceDTORequest req) {
-        tgBot.onWebInterfaceRequest(req);
+    public ResponseEntity<String> generate(@RequestBody String req) {
+        try {
+            tgBot.onWebInterfaceRequest(mapper.readValue(req, InterfaceDTORequest.class));
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
         return ResponseEntity.ok().build();
     }
 }
