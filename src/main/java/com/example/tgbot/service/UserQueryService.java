f4292ecb57d8c0e490cfb8c96edb4eda6f6b89ae
Konstantin <kostos97@gmail.com>
2026-03-07 19:52:47 +0300

    - Правка бага с новыми юзерами

--- src/main/java/com/example/tgbot/service/UserQueryService.java
+++ src/main/java/com/example/tgbot/service/UserQueryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 /**
@@ -36,6 +37,10 @@ public class UserQueryService {
                     User newUser = User.builder()
                             .telegramId(chatId)
                             .balance(0)
+                            .ambassador(false)
+                            .ambassadorProfitCoefficient(BigDecimal.ZERO)
+                            .ambassadorProfitTotal(BigDecimal.ZERO)
+                            .balanceHold(0)
                             .build();
                     return userRepository.save(newUser);
                 });
