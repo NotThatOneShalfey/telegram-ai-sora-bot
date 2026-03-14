80bc4c2f7eb2b8c9215cd654a324e3837ea56280
Konstantin <kostos97@gmail.com>
2026-03-14 14:01:55 +0300

    - Проверка абуза с подарком, отключение только фиксированных пользователей для Dev билда

--- src/main/java/com/example/tgbot/telegram/TgBot.java
+++ src/main/java/com/example/tgbot/telegram/TgBot.java
@@ -119,9 +119,9 @@ public class TgBot extends TelegramWebhookBot {
                 log.trace("update has CallbackQuery");
                 User user = userService.findOrCreateUser(update.getCallbackQuery().getMessage().getChatId());
                 // Проверка на дев билд
-                if (devBuild && (user.getId() != 1 && user.getId() != 2 && user.getId() != 4 && user.getId() != 27)) {
-                    return;
-                }
+//                if (devBuild && (user.getId() != 1 && user.getId() != 2 && user.getId() != 4 && user.getId() != 27)) {
+//                    return;
+//                }
                 UserSession userSession = sessions.computeIfAbsent(update.getCallbackQuery().getMessage().getChatId().toString(), k -> new UserSession(user));
                 userSession.touch();
                 callbackHandler.handleCallback(update.getCallbackQuery(), userSession);
